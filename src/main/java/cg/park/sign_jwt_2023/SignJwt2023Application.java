5d12cd6e02e61914c817d792ddc7eb479c06a2a0
pcg0902 <qkrcksrbs8@naver.com>
2023-01-17 11:02:02 +0900

    [DB] 연동 해제

--- src/main/java/cg/park/sign_jwt_2023/SignJwt2023Application.java
+++ src/main/java/cg/park/sign_jwt_2023/SignJwt2023Application.java
@@ -2,8 +2,9 @@ package cg.park.sign_jwt_2023;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class SignJwt2023Application {
 
 	public static void main(String[] args) {
